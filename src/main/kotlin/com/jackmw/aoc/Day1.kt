5263e973dce2b7b6ca1ac5e029c483906571e856
Jack MW <jack@faire.com>
2021-12-02 01:00:47 -0500

    simplified file structure

--- src/main/kotlin/com/jackmw/aoc/Day1.kt
+++ src/main/kotlin/com/jackmw/aoc/Day1.kt
@@ -1,6 +1,20 @@
-package com.jackmw.aoc.day1
+package com.jackmw.aoc
 
-import com.jackmw.aoc.LineWorker
+fun main(args: Array<String>) {
+  part1("day1/part1-test.txt")
+  part1("day1/part1-input.txt" )
+
+  part2("day1/part2-test.txt")
+  part2("day1/part2-input.txt")
+}
+
+private fun part1(fileName: String) {
+  println("$fileName -> ${ Counter().part1(fileName)}")
+}
+
+private fun part2(fileName: String) {
+  println("$fileName -> ${ Counter().part2(fileName)}")
+}
 
 class Counter : LineWorker<Int, Int>() {
   private fun countWithWindow(inputs: List<Int>, windowSize: Int): Int {
--- src/main/kotlin/com/jackmw/aoc/Day2.kt
+++ src/main/kotlin/com/jackmw/aoc/Day2.kt
@@ -1,6 +1,22 @@
-package com.jackmw.aoc.day2
+package com.jackmw.aoc
 
-import com.jackmw.aoc.LineWorker
+fun main(args: Array<String>) {
+  part1("day2/part1-test.txt")
+  part1("day2/part1-input.txt")
+
+  part2("day2/part1-test.txt")
+  part2("day2/part1-input.txt")
+}
+
+private fun part1(fileName: String) {
+  val (horizontal, vertical) = Driver().part1(fileName)
+  println("$fileName -> ($horizontal, $vertical) = ${horizontal * vertical}")
+}
+
+private fun part2(fileName: String) {
+  val (horizontal, vertical) = Driver().part2(fileName)
+  println("$fileName -> ($horizontal, $vertical) = ${horizontal * vertical}")
+}
 
 class Driver : LineWorker<Driver.Instruction, Pair<Int, Int>>() {
   data class Instruction(

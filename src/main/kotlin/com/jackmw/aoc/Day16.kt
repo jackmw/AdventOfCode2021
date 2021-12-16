edfb2c6465df98d5b7213b47cd1c7cca9f6834bc
Jack MW <jack@faire.com>
2021-12-16 15:46:10 -0500

    day 16

--- src/main/kotlin/com/jackmw/aoc/Day16.kt
+++ src/main/kotlin/com/jackmw/aoc/Day16.kt
@@ -4,25 +4,32 @@ import com.jackmw.aoc.Utils.Companion.readFileAsLines
 import java.util.PriorityQueue
 
 fun main() {
-  println(Bits.parsePackets("D2FE28"))
-  println(Bits.parsePackets("38006F45291200"))
-  println(Bits.parsePackets("EE00D40C823060"))
-  println(Bits.parsePackets("8A004A801A8002F478"))
-  println(Bits.parsePackets("620080001611562C8802118E34"))
-  println(Bits.parsePackets("C0015000016115A2E0802F182340"))
-  println(Bits.parsePackets("A0016C880162017C3686B18A3D4780"))
+  println(Bits.parsePacket("D2FE28"))
+  println(Bits.parsePacket("38006F45291200"))
+  println(Bits.parsePacket("EE00D40C823060"))
+  println(Bits.parsePacket("8A004A801A8002F478"))
+  println(Bits.parsePacket("620080001611562C8802118E34"))
+  println(Bits.parsePacket("C0015000016115A2E0802F182340"))
+  println(Bits.parsePacket("A0016C880162017C3686B18A3D4780"))
   println()
   part1("/day16/part1-test.txt")
   part1("/day16/part1-input.txt")
+
+
+  println("Part 2")
+  part2("/day16/part1-test.txt")
+  part2("/day16/part1-input.txt")
 }
 
 
 private fun part1(fileName: String) {
-  val packets = Bits.parsePackets(readFileAsLines(fileName).first().trim().uppercase())
-  println("$fileName -> sum of version: ${packets.sumOf { it.getSumOfVersion() }}")
+  val (packet, _) = Bits.parsePacket(readFileAsLines(fileName).first().trim().uppercase())
+  println("$fileName -> sum of version: ${packet.getSumOfVersion()}}")
 }
 
 private fun part2(fileName: String) {
+  val (packet, _) = Bits.parsePacket(readFileAsLines(fileName).first().trim().uppercase())
+  println("$fileName -> evaluation: ${packet.evaluate()}}")
 }
 
 class Bits {
@@ -31,17 +38,23 @@ class Bits {
     val version: Int,
     val type: Int,
   ) {
-    open fun getSumOfVersion(): Int {
-      return version
+    open fun getSumOfVersion(): Long {
+      return version.toLong()
     }
+
+    abstract fun evaluate(): Long
   }
 
   class LiteralPacket(
     version: Int,
-    private val content: Int,
+    private val content: Long,
   ): Packet(version, type = PACKET_TYPE_LITERAL) {
     override fun toString(): String {
       return "[version=$version, type=$type, content=$content]"
+    }
+
+    override fun evaluate(): Long {
+      return content
     }
   }
 
@@ -54,8 +67,21 @@ class Bits {
       return "[version=$version, type=$type, subPackets=$subPackets]"
     }
 
-    override fun getSumOfVersion(): Int {
+    override fun getSumOfVersion(): Long {
       return this.version + subPackets.sumOf { it.getSumOfVersion() }
+    }
+
+    override fun evaluate(): Long {
+      return when (type) {
+        0 -> subPackets.sumOf { it.evaluate() }
+        1 -> subPackets.asSequence().map { it.evaluate() }.reduce { acc, l -> acc * l }
+        2 -> subPackets.minOf { it.evaluate() }
+        3 -> subPackets.maxOf { it.evaluate() }
+        5 -> if (subPackets[0].evaluate() > subPackets[1].evaluate()) 1 else 0
+        6 -> if (subPackets[0].evaluate() < subPackets[1].evaluate()) 1 else 0
+        7 -> if (subPackets[0].evaluate() == subPackets[1].evaluate()) 1 else 0
+        else -> throw IllegalStateException("Invalid type $type")
+      }
     }
   }
 
@@ -81,11 +107,11 @@ class Bits {
       'F' to listOf('1', '1', '1', '1'),
     )
 
-    fun parsePackets(input: String): List<Packet> {
+    fun parsePacket(input: String): Pair<Packet, Int> {
       val chars = input.flatMap { char -> HEX_TO_BIN[char]!! }
-      val (packets, _) = parsePackets(chars, 0)
-      println("$input sumOfVersions=${packets.sumOf { it.getSumOfVersion() }}")
-      return packets
+      val (packet, index) = parsePacket(chars, 0)
+      println("$input sumOfVersions=${packet.getSumOfVersion()}}")
+      return packet to index
     }
 
     private fun parsePackets(chars: List<Char>, startIndex: Int): Pair<List<Packet>, Int> {
@@ -106,9 +132,9 @@ class Bits {
 
     private fun parsePacket(chars: List<Char>, startIndex: Int): Pair<Packet, Int> {
       var index = startIndex
-      val version = charBinToInt(chars.subList(index, index + 3))
+      val version = charBinToInt(chars.subList(index, index + 3)).toInt()
       index += 3
-      val type = charBinToInt(chars.subList(index, index + 3))
+      val type = charBinToInt(chars.subList(index, index + 3)).toInt()
       index += 3
       if (type == PACKET_TYPE_LITERAL) {
         val (content, newIndex) = parseLiteralPacketContent(chars, index)
@@ -117,30 +143,34 @@ class Bits {
       } else {
         val lengthType = chars[index]
         index++
-        if (lengthType == '0') {
-          val subPacketLength = charBinToInt(chars.subList(index, index + 15))
-          index += 15
-          val subPacketContent = chars.subList(index, index + subPacketLength)
-          index += subPacketLength
-          val (subPackets, _) = parsePackets(subPacketContent, 0)
-          return OperatorPacket(version, type, subPackets) to index
-        } else if (lengthType == '1') {
-          val subPacketCount = charBinToInt(chars.subList(index, index + 11))
-          index += 11
-          val subPackets = mutableListOf<Packet>()
-          for (count in 1 .. subPacketCount) {
-            val (subPacket, newIndex) = parsePacket(chars, index)
-            subPackets.add(subPacket)
-            index = newIndex
+        when (lengthType) {
+          '0' -> {
+            val subPacketLength = charBinToInt(chars.subList(index, index + 15)).toInt()
+            index += 15
+            val subPacketContent = chars.subList(index, index + subPacketLength)
+            index += subPacketLength
+            val (subPackets, _) = parsePackets(subPacketContent, 0)
+            return OperatorPacket(version, type, subPackets) to index
           }
-          return OperatorPacket(version, type, subPackets) to index
-        } else {
-          throw IllegalStateException("Invalid length type $lengthType")
+          '1' -> {
+            val subPacketCount = charBinToInt(chars.subList(index, index + 11))
+            index += 11
+            val subPackets = mutableListOf<Packet>()
+            for (count in 1 .. subPacketCount) {
+              val (subPacket, newIndex) = parsePacket(chars, index)
+              subPackets.add(subPacket)
+              index = newIndex
+            }
+            return OperatorPacket(version, type, subPackets) to index
+          }
+          else -> {
+            throw IllegalStateException("Invalid length type $lengthType")
+          }
         }
       }
     }
 
-    private fun parseLiteralPacketContent(chars: List<Char>, startIndex: Int): Pair<Int, Int> {
+    private fun parseLiteralPacketContent(chars: List<Char>, startIndex: Int): Pair<Long, Int> {
       var index = startIndex
       val contents = mutableListOf<Char>()
       while (chars[index] == '1') {
@@ -153,11 +183,11 @@ class Bits {
       return charBinToInt(contents) to index
     }
 
-    private fun charBinToInt(chars: List<Char>): Int {
+    private fun charBinToInt(chars: List<Char>): Long {
       val parseChars = chars.toMutableList()
       parseChars.add(0, '0')
       // println("JACK_DEBUG parsing $parseChars to dec")
-      return String(parseChars.toCharArray()).toInt(2)
+      return String(parseChars.toCharArray()).toLong(2)
     }
   }
 }

d0ab9423e2fbd335f104ec6b0eabadc0b0b502ca
Jack MW <jack.wang@faire.com>
2022-12-19 12:29:48 -0500

    solved 16 part 1, part 2 is too slow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.7.21"
     application
 }
 
@@ -13,6 +13,7 @@ repositories {
 }
 
 dependencies {
+    implementation("com.github.shiguruikai:combinatoricskt:1.6.0")
     testImplementation(kotlin("test"))
 }
 
--- src/main/kotlin/com/jackmw/aoc2022/Day16.kt
+++ src/main/kotlin/com/jackmw/aoc2022/Day16.kt
@@ -1,27 +1,37 @@
 package com.jackmw.aoc2022
 
 import com.jackmw.Utils
+import kotlin.math.sign
 
 fun main(args: Array<String>) {
   val day = Day16()
   day.part1("/2022/day16/part1-test.txt")
+  day.part1("/2022/day16/part1-input.txt")
+
+  day.part2("/2022/day16/part1-test.txt")
+  day.part2("/2022/day16/part1-input.txt")
 }
 
-class Day16 {
+internal class Day16 {
   internal fun part1(fileName: String) {
     val valves = parseFile(fileName)
-    for (valve in valves) {
-      println(valve)
-    }
     val gameMap = GameMap(valves)
-    val bestRoute = gameMap.findBestRoute(steps = 30, startAt = "AA")
-    println(bestRoute)
-    gameMap.printRoute(bestRoute, 30)
+    // val bestRoute = gameMap.findBestPathForPressureDfs(30, "AA")
+    val mostPressureReleased = gameMap.findPaths("AA", 30)
+    println("most pressure released is $mostPressureReleased")
+  }
+
+  internal fun part2(fileName: String) {
+    val valves = parseFile(fileName)
+    val gameMap = GameMap(valves)
+    val mostPressureReleased = gameMap.findPaths2("AA", elephantFrom = "AA", 30)
+    println("part2: most pressure released is $mostPressureReleased")
   }
 
   internal data class Edge(
     val fromValve: Valve,
     val toValve: Valve,
+    val fromIsOpen: Boolean = false,
   ) {
     val isOpening = fromValve == toValve
 
@@ -36,6 +46,120 @@ class Day16 {
 
   internal class GameMap(private val valves: List<Valve>) {
     private val valveMap = valves.associateBy { it.name }
+    private val knownPaths = mutableMapOf<Pair<String, String>, List<String>>()
+    private val operableValves = valves.filter { it.flowRate > 0 }.map { it.name }.toSet()
+
+    private fun <T> Set<T>.allPairs(): List<Pair<T, T>> {
+      val permutations = mutableListOf<Pair<T, T>>()
+      for (first in this) {
+        for (second in this) {
+          if (first != second) {
+            permutations.add(Pair(first, second))
+          }
+        }
+      }
+      return permutations
+    }
+    fun findPaths2(
+      myFrom: String?,
+      elephantFrom: String?,
+      totalSteps: Int,
+      myVisited: Set<String> = setOf(),
+      elephantVisited: Set<String> = setOf(),
+      myStepsSoFar: Int = 0,
+      elephantStepsSoFar: Int = 0,
+      releasedPressure: Long = 0,
+    ): Long {
+      if (myFrom == null && elephantFrom == null) {
+        throw IllegalStateException("don't know how we got here..")
+      }
+      else if (myFrom == null && elephantFrom != null) {
+        return findPaths(
+          elephantFrom,
+          totalSteps = totalSteps,
+          visited = elephantVisited + myVisited,
+          stepsSoFar = elephantStepsSoFar,
+          releasedPressure = releasedPressure,
+        )
+      } else if (myFrom != null && elephantFrom == null) {
+        return findPaths(
+          myFrom,
+          totalSteps = totalSteps,
+          visited = elephantVisited + myVisited,
+          stepsSoFar = myStepsSoFar,
+          releasedPressure = releasedPressure,
+        )
+      }
+      // println("Generating and evaluating open order for ${operableValves.size} operable valves.")
+      val nextValves = operableValves - myVisited - elephantVisited
+      return nextValves.allPairs().asSequence()
+        // +1 is the cost of openning the next valve
+        .filter { (myNext, elephantNext) ->
+          myStepsSoFar + getSteps(myFrom!!, myNext) + 1 < totalSteps ||
+              elephantStepsSoFar + getSteps(elephantFrom!!, elephantNext) + 1 < totalSteps
+        }
+        .maxOfOrNull { (myNext, elephantNext) ->
+          val myStepCountToOpenNextValve = myStepsSoFar + 1 + getSteps(myFrom!!, myNext)
+          val elephantStepCountToOpenNextValve = elephantStepsSoFar + 1 + getSteps(elephantFrom!!, elephantNext)
+          val myNextFrom = if (myStepCountToOpenNextValve < totalSteps) myNext else null
+          val elephantNextFrom = if (elephantStepCountToOpenNextValve < totalSteps) elephantNext else null
+          findPaths2(
+            myFrom = myNextFrom,
+            elephantFrom = elephantNextFrom,
+            totalSteps = totalSteps,
+            myVisited = myVisited + setOfNotNull(myNextFrom),
+            elephantVisited = elephantVisited + setOfNotNull(elephantNextFrom),
+            myStepsSoFar = myStepCountToOpenNextValve,
+            elephantStepsSoFar = elephantStepCountToOpenNextValve,
+            releasedPressure = releasedPressure +
+                (myNextFrom?.let { valveMap[it]!!.flowRate } ?: 0L) * (totalSteps - myStepCountToOpenNextValve) +
+                (elephantNextFrom?.let { valveMap[it]!!.flowRate } ?: 0L)* (totalSteps - elephantStepCountToOpenNextValve)
+          )
+        } ?: releasedPressure
+    }
+
+    fun findPaths(
+      from: String,
+      totalSteps: Int,
+      visited: Set<String> = setOf(),
+      stepsSoFar: Int = 0,
+      releasedPressure: Long = 0,
+    ): Long {
+      // println("Generating and evaluating open order for ${operableValves.size} operable valves.")
+      val nextValves = operableValves - visited
+      return nextValves.asSequence()
+        // +1 is the cost of openning the next valve
+        .filter { stepsSoFar + getSteps(from, it) + 1 < totalSteps }
+        .maxOfOrNull { nextValve ->
+          val stepCountToOpenNextValve = stepsSoFar + 1 + getSteps(from, nextValve)
+          findPaths(
+            from = nextValve,
+            totalSteps = totalSteps,
+            visited = visited + nextValve,
+            stepsSoFar = stepCountToOpenNextValve,
+            releasedPressure = releasedPressure + (totalSteps - stepCountToOpenNextValve) * valveMap[nextValve]!!.flowRate
+          )
+        } ?: releasedPressure
+    }
+
+    private fun scoreOpenOrder(valveOpenOrder: List<String>, totalSteps: Int, startingAt: String): Pair<Long, List<String>> {
+      val completePath = mutableListOf<String>()
+      var lastValve: String = startingAt
+      var releasedPressure = 0L
+      completePath.add(startingAt)
+      for(valve in valveOpenOrder) {
+        val pathToValve = getPath(lastValve, valve).drop(1)
+        if (completePath.size + pathToValve.size + 1 <= totalSteps) {
+          completePath.addAll(pathToValve)
+          completePath.add(valve)
+          releasedPressure += valveMap[valve]!!.flowRate * (totalSteps - completePath.size + 1)
+        } else {
+          break
+        }
+        lastValve = valve
+      }
+      return releasedPressure to completePath
+    }
 
     private fun getEdges(valve: Valve): List<Edge> {
       val edges = mutableListOf<Edge>()
@@ -46,73 +170,130 @@ class Day16 {
       return edges
     }
 
-    fun findBestRoute(steps: Int, startAt: String="AA"): List<Edge> {
-      val startingEdges = getEdges(valveMap[startAt]!!)
-      var bestScore: Long = 0
-      var bestSteps: List<Edge> = listOf()
-      for (startingEdge in startingEdges) {
-        val (score, routeStep) = findBestRoute(steps, startingEdge)
-        if (score < bestScore) {
-          bestScore = score
-          bestSteps = routeStep
-        }
-      }
-      return bestSteps
+    private fun getVisitCount(valve: Valve): Int {
+      return (if (valve.flowRate > 0) 1 else 0) + valve.neighbours.size
     }
 
-    private fun findBestRoute(steps: Int, startingEdge: Edge): Pair<Long, List<Edge>> {
-      var remainingSteps = steps
-      val distance = mutableMapOf<Edge, Long>()
-      val previous = mutableMapOf<Edge, Edge?>()
-      val unvisited = mutableSetOf<Edge>()
-      val visited = mutableSetOf<Edge>()
-      val score = mutableMapOf<Edge, Long>()
-
-      val edges = valves.flatMap { getEdges(it) }.toSet()
-      for (edge in edges) {
-        distance[edge] = Long.MAX_VALUE
-        previous[edge] = null
-        unvisited.add(edge)
+    private fun buildPath(start: String, end: String, previous: Map<String, String?>): List<String> {
+      val valves = mutableListOf<String>()
+      valves.add(end)
+      var valve: String? = previous[end]
+      while (valve != null) {
+        valves.add(0, valve)
+        if (valve == start) {
+          break
+        }
+        valve = previous[valve]
       }
-      val opennedValves = mutableMapOf<Valve, Int>()
-      distance[startingEdge] = 0
+      return valves
+    }
+
+    private fun getSteps(start: String, end: String): Int {
+      if (start == end) return 0
+      val knownPath = knownPaths[start to end]
+      if (knownPath != null) {
+        return knownPath.size - 1
+      }
+      val shortestPath = findShortestPath(start, end)
+      return if (shortestPath.isEmpty()) {
+        throw IllegalStateException("No path from $start to $end.")
+      } else {
+        knownPaths[start to end] = shortestPath
+        shortestPath.size - 1
+      }
+    }
+
+    private fun getPath(start: String, end: String): List<String> {
+      if (start == end) return listOf()
+      val knownPath = knownPaths[start to end]
+      if (knownPath != null) {
+        return knownPath
+      }
+      val shortestPath = findShortestPath(start, end)
+      return if (shortestPath.isEmpty()) {
+        listOf()
+      } else {
+        knownPaths[start to end] = shortestPath
+        shortestPath
+      }
+    }
+
+    private fun findShortestPath(start: String, end: String): List<String> {
+      val distance = mutableMapOf<String, Int>()
+      val previous = mutableMapOf<String, String?>()
+
+      val unvisited = mutableSetOf<String>()
+      for (valve in valves.map { it.name }) {
+        distance[valve] = Int.MAX_VALUE
+        previous[valve] = null
+        unvisited.add(valve)
+      }
+      distance[start] = 0
 
       while (unvisited.isNotEmpty()) {
-        val currentEdge = unvisited.minBy { distance[it]!! }
-        unvisited.remove(currentEdge)
-        val distanceToMe = distance[currentEdge]!!
-        if (currentEdge.isOpening) {
-          opennedValves[currentEdge.fromValve] = remainingSteps - 1
-        }
-        score[currentEdge] = calculateScore(opennedValves)
-        remainingSteps--
+        val valve = unvisited.minBy { distance[it]!! }
+        val distanceToMe = distance[valve]!!
+        unvisited.remove(valve)
 
-        val possibleEdges = getEdges(currentEdge.toValve).intersect(unvisited)
-        for (edge in possibleEdges) {
-          val distanceToNeighbour = distance[edge]!!
-          val newDistance = distanceToMe + 1
-          if (newDistance < distanceToNeighbour) {
-            distance[edge] = newDistance
-            previous[edge] = currentEdge
+        if (valve == end) {
+          return buildPath(start, end, previous)
+        }
+
+        val neighbours = valveMap[valve]!!.neighbours
+        for (neighbour in neighbours) {
+          if (neighbour in unvisited) {
+            val newDistance = distanceToMe + 1
+            val distanceToNeighbour = distance[neighbour]!!
+            if (newDistance < distanceToNeighbour) {
+              distance[neighbour] = newDistance
+              previous[neighbour] = valve
+            }
           }
         }
-        1 + 1
       }
-      val lastEdge = score.maxBy { it.value }
-      val lastStep = lastEdge.key
-      val allSteps = mutableListOf(lastStep)
-      var step: Edge? = previous[lastStep]
-      while (step != null) {
-        allSteps.add(0, step)
-        step = previous[step]
-      }
-      return lastEdge.value to allSteps
+      return listOf()
     }
 
-    private fun calculateScore(opennedValves: Map<Valve, Int>): Long {
-      return opennedValves.asSequence()
-        .map { (valve, openSteps) -> valve.flowRate * openSteps }
-        .sum()
+    fun findBestRouteBruteForce(steps: Int = 30, startingAt: String = "AA"): List<Edge> {
+      val maxVisitCount = valves.associate { it.name to getVisitCount(it) }
+      val unexplored = getEdges(valve = valveMap[startingAt]!!)
+        .map { listOf(it) }
+        .toMutableList()
+      val paths: MutableList<List<Edge>> = mutableListOf()
+      println("Exploring paths")
+      while (unexplored.isNotEmpty()) {
+        val path = unexplored.removeAt(0)
+        val nextEdges = getEdges(path.last().toValve)
+        var pathIsComplete = true
+        val pathVisitCount = getVisitedCount(path)
+        for (edge in nextEdges) {
+          if (pathVisitCount[edge.toValve.name] == maxVisitCount[edge.toValve.name]) {
+            continue
+          }
+          val newPath = path + edge
+          unexplored.add(newPath)
+          pathIsComplete = false
+        }
+        if (pathIsComplete) {
+          paths.add(path)
+        }
+      }
+      println("Scoring ${paths.size} paths")
+      var bestScore: Long = 0
+      var bestPath: List<Edge> = listOf()
+      for (path in paths) {
+        val score = scorePath(steps, path)
+        if (score > bestScore) {
+          bestScore = score
+          bestPath = path
+        }
+      }
+      println("Found best path with score $bestScore")
+      return bestPath
+    }
+
+    private fun getVisitedCount(path: List<Edge>): Map<String, Int> {
+      return path.groupingBy { it.toValve.name }.eachCount()
     }
 
     fun printRoute(edges: List<Edge>, steps: Int) {
@@ -147,13 +328,21 @@ class Day16 {
       }
       println("Released $totalPressureReleased in $steps minutes.")
     }
+
+    companion object {
+      private fun scorePath(steps: Int, path: List<Edge>): Long {
+        var remainingSteps = steps
+        var releasedPressure = 0L
+        for (edge in path) {
+          if (edge.isOpening) {
+            releasedPressure += (remainingSteps - 1) * edge.toValve.flowRate
+          }
+          remainingSteps--
+        }
+        return releasedPressure
+      }
+    }
   }
-
-
-
-  internal fun part2(fileName: String) {
-  }
-
 
   internal data class Valve(
     val name: String,
